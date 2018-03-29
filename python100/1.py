43c272bda8128a629d71d8f539b59323ed9a9f33
Sunflowerseed <496092102@qq.com>
2018-03-29 12:34:33 +0800

    repair

--- python100/1.py
+++ python100/1.py
@@ -1,5 +1,5 @@
-for i in (1,5):
-    for j in (1,4)):
-        for k in (1,5)):
-            while i!=j&i!=k&j!=k:
+for i in range(1,5):
+    for j in range(1,5):
+        for k in range(1,5):
+            if (i!=k and i!=j and k!=j):  
                 print(i,j,k)
